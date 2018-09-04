d0315cd6b3a10158b94b733b4726827c7b89814c
gellys <anng.nascimento@gmail.com>
2018-09-04 17:53:39 -0300

    Primeira versão do login

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// VALIDACOES DO FORMULARIO
+
+function validation(){
+
+  // Username e password validos
+   var userName = "Brasil";
+   var userPass = "2018";
+   
+   // Pega username de entrada ;
+   var user = document.getElementById("name").value;
+   var pass = document.getElementById("pass").value;
+   
+   // Condições do formulario
+   if(user==userName && pass==userPass){
+    alert("Cadastro efetuado com sucesso!");
+     return true;
+   }else if(user=="" && pass==""){
+     alert("Digite usuário e senha pra continuar");
+     return false;
+   }
+   else{
+     alert("A senha e usuário está incorreto");
+     return false;
+   }
+ }
+
+ "use strict";  
+ var switchText = document.getElementById("help");  
+ var elToggled = document.getElementById("01");
+ 
+ elToggled.style.display = "block";  
+ switchText.addEventListener("click", function(){  
+     if(elToggled.style.display == "block") {  
+         elToggled.style.display = "none";  
+     } else {  
+         elToggled.style.display = "block";  
+     }  
+ }, false);  
